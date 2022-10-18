d45b710a8d7ddbe216759dd49b0fa51e49f6d292
chisainty <johnchinedudivine@gmail.com>
2022-10-18 20:57:16 +0100

    first commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,11 +15,10 @@ void print_to_98(int n)
 		while (n > 98)
 		{
 			printf("%d, ", n--);
-                        
 		}
                 printf("%d\n", n);
 	}
-	else 
+	else
 	{
 		while (n < 98)
 		{
